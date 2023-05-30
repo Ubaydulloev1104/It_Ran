58e8744f0c083d0ccc10e0e56911ec863a2958b0
Ubaydulloev1104 <116807178+Ubaydulloev1104@users.noreply.github.com>
2023-05-30 14:14:20 +0500

    init

---
+++ Ubaydulloev_A_dz_1/Dey_1/task_2/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.WriteLine(" Это программа предназначино для вычисления квадратные уровнений аx^2+bx+c=0 ");
+while (true)
+{
+    double a;
+    double b;
+    double c;
+    Console.Write("Вводите a=");
+    bool isnumber_a = double.TryParse(Console.ReadLine(), out a);
+    if (isnumber_a)
+    {
+        Console.Write("Вводите b=");
+        bool isnumber_b = double.TryParse(Console.ReadLine(), out b);
+        if (isnumber_b)
+        {
+            Console.Write("Вводите c=");
+            bool isnumber_c = double.TryParse(Console.ReadLine(), out c);
+
+            if (isnumber_c)
+            {
+                double d = (b * b) - 4 * a * c;
+                if (d > 0)
+                {
+                    double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                    Console.WriteLine($"Уравнение имеет два кроня : X1={x1} , X2={x2} ");
+                }
+                else if (d == 0)
+                {
+                    double x = -b / (2 * a);
+                    Console.WriteLine($"Уравнение имеет один кронь : X={x}");
+                }
+                else
+                {
+                    Console.WriteLine("Уравнение не имеет действительные кроней");
+                }
+                break;
+            }
+            else
+            {
+                Console.WriteLine(" а должин бить числом ");
+            }
+        }
+        else
+        {
+            Console.WriteLine(" а должин бить числом ");
+        }
+    }
+    else
+    {
+        Console.WriteLine(" а должин бить числом ");
+    }
+}
