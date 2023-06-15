a4f84c798a81f11f5dd9044744a629c11847131a
ubaydulloev7343344 <ubaydulloev.1104@gmal.com>
2023-06-15 12:44:02 +0500

    Home work 6

---
+++ Dey_6/HashSetTask/Program.cs
@@ -0,0 +1,15 @@
+﻿HashSet<string> playersSet = new HashSet<string>();
+playersSet.Add("Ter Stegen");
+playersSet.Add("Pique");
+playersSet.Add("Alves");
+playersSet.Add("Arauhjo");
+playersSet.Add("Jordi Alba");
+playersSet.Add("De Jong");
+playersSet.Add("Busquets");
+playersSet.Add("Gavi");
+string[] playersArray = new string[7] { "De Jong", "Busquets", "Gavi", "Dembele", "Aubemiyang", "Ferran", "Adama" };
+var commonPlayers = playersSet.Intersect(playersArray);
+foreach (var player in commonPlayers)
+{
+    Console.WriteLine(player);
+}
